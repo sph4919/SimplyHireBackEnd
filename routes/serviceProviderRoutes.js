c1bc0937e847de092f028166abc915f334f92aed
sph4919 <patelsugnaykumar492003@gmail.com>
2026-01-23 22:45:26 -0600

    fixing dashboard

--- routes/serviceProviderRoutes.js
+++ routes/serviceProviderRoutes.js
@@ -107,10 +107,10 @@ router.get('/listServices', (req, res) => {
 });
 
 //dashboard 
-router.get('/serviceDashboardFetch', (req, res) => {
+router.get('/serviceDashboardFetch/:providerId', (req, res) => {
 
 
-  let id = req.session.serviceProId;
+  let id = req.params.providerId;
   let obj = [id];
     const query = 'SELECT request_id, description , address , city , state , zip , status  FROM request WHERE serviceprovider_id = ?';
     db.query(query,obj,(err, results) => {
